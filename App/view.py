2cad30283f72b79202690e2a9bf287e17fed88f5
alejandra-melo <a.melo4@uniandes.edu.co>
2021-02-04 16:19:59 -0500

    cambios 1-3 carga de archivos

--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TO-DO: Modificación de Est-1 en el Lab 2
+    print("3- Cargar Book-Tags!")
     print("0- Salir")
 
 
@@ -63,7 +63,7 @@ def loadBookTags():
     """
     Cargar los Tags de libros
     """
-    # TO-DO: Modificación de Est-1 en el Lab 2
+    print('Primer libro cargado: ' + str(lt.firstElement(books)))
 
 
 """
